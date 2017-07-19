14a6cc240ffe6ebec300c0bd61138c5c393f829b
Owen Yip <owen.yip@dreamover-studio.com>
2017-07-19 18:55:30 +0800

    update

--- src/ios/ELCImagePicker/ELCImagePickerController.m
+++ src/ios/ELCImagePicker/ELCImagePickerController.m
@@ -49,8 +49,8 @@
 {
     BOOL shouldSelect = previousCount < self.maximumImagesCount;
     if (!shouldSelect) {
-        NSString *title = [NSString stringWithFormat:NSLocalizedString(@"Maximum %d photos.", nil), self.maximumImagesCount];
-        NSString *message = [NSString stringWithFormat:NSLocalizedString(@"You can only select %d photos at a time.", nil), self.maximumImagesCount];
+        NSString *title = [NSString stringWithFormat:NSLocalizedString(@"最多%d张照片", nil), self.maximumImagesCount];
+        NSString *message = [NSString stringWithFormat:NSLocalizedString(@"您一次最多只能选择％d张照片。", nil), self.maximumImagesCount];
         [[[UIAlertView alloc] initWithTitle:title
                                     message:message
                                    delegate:nil
